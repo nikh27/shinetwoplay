8b776889a8e7c56c931ce6bae909e64669fd2a70
Nikhil Pandey <npqwerty93@gmail.com>
2026-02-18 02:37:23 +0530

    voice chat feautre is fix

--- shinetwoplay/rooms/consumers.py
+++ shinetwoplay/rooms/consumers.py
@@ -339,8 +339,6 @@ class RoomConsumer(AsyncWebsocketConsumer):
                 'webrtc_offer': self.handle_webrtc_signal,
                 'webrtc_answer': self.handle_webrtc_signal,
                 'webrtc_ice': self.handle_webrtc_signal,
-                # Voice chat audio relay
-                'audio_stream': self.handle_audio_stream,
             }
             
             handler = handlers.get(event)
@@ -931,6 +929,17 @@ class RoomConsumer(AsyncWebsocketConsumer):
             }
         )
 
+    async def broadcast_webrtc_signal(self, event):
+        """Send WebRTC signal to client (skip sender)"""
+        if event['sender'] != self.username:
+            await self.send(text_data=json.dumps({
+                'event': event['signal_event'],
+                'data': {
+                    'sender': event['sender'],
+                    'payload': event['payload'],
+                }
+            }))
+
     # ============= Owner Management Handlers =============
 
     async def handle_transfer_ownership(self, data):
@@ -1360,28 +1369,6 @@ class RoomConsumer(AsyncWebsocketConsumer):
             }
         }))
 
-    async def handle_audio_stream(self, data):
-        """Relay audio stream data to other players"""
-        # Broadcast to room (excluding sender)
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'broadcast_audio_stream',
-                'sender': self.username,
-                'audio': data.get('audio')
-            }
-        )
-
-    async def broadcast_audio_stream(self, event):
-        """Send audio stream to client (skip sender)"""
-        if event['sender'] != self.username:
-            await self.send(text_data=json.dumps({
-                'event': 'audio_stream',
-                'data': {
-                    'sender': event['sender'],
-                    'audio': event['audio']
-                }
-            }))
 
     async def send_error(self, code, message):
         """Send error message"""
